c1749d75310de2175ed8ac22c7272b6322a80ee5
Vishva  Sudantha <vishva.sudantha@gmail.com>
2020-09-22 13:31:27 +0800

    Updates config script

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # =
-mysqlUsername=$1
-mysqlPass=$2
-mysqlDB=$3
+mysqlUsername=${1:-root}
+mysqlPass=${2:-24tz3wgF7vpJhkYG}
+mysqlDB=${3:-istdbigdata}
 # =
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
@@ -11,7 +11,8 @@ else
   sudo apt-get install -y mysql-server
 echo "CREATE USER '$mysqlUsername'@'%' IDENTIFIED BY ""' '$mysqlPass';
       GRANT ALL PRIVILEGES ON *.* to '$mysqlUsername'@'%'
-      FLUSH PRIVILEGES;" | sudo mysql -u root
+      FLUSH PRIVILEGES;
+      CREATE DATABASE IF NOT EXISTS '$mysqlDB';" | sudo mysql -u root
 sudo cp my.cnf /etc/mysql/my.cnf
 sudo service mysql stop
 sudo service mysql start
