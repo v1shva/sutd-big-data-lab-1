d9287b256806adaf31e97a89c5e7af6b576a80eb
Vishva  Sudantha <vishva.sudantha@gmail.com>
2020-09-22 13:22:58 +0800

    Updates my.cnf

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 # =
-mysqlRootPass=$1
-mysqlDB=$2
+mysqlUsername=$1
+mysqlPass=$2
+mysqlDB=$3
 # =
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
 else
   sudo apt-get update
   sudo apt-get install -y mysql-server
-  echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysqlRootPass';
-        UPDATE mysql.user SET Host='%' WHERE Host='localhost' AND User='root';
-        UPDATE mysql.db SET Host='%' WHERE Host='localhost' AND User='root';
-        FLUSH PRIVILEGES;" | sudo mysql -u root
-  sudo cp my.cnf /etc/mysql/my.cnf
-  sudo service mysql stop
-  sudo service mysql start
-  #ssh ubuntu@$mysqlIP -i ./key.pem sudo apt install -y mysql-client-5.7
-  echo "create database $mysqlDB;" | mysql -u root -p$mysqlRootPass
-fi
+echo "CREATE USER '$mysqlUsername'@'%' IDENTIFIED BY ""' '$mysqlPass';
+      GRANT ALL PRIVILEGES ON *.* to '$mysqlUsername'@'%'
+      FLUSH PRIVILEGES;" | sudo mysql -u root
+sudo cp my.cnf /etc/mysql/my.cnf
+sudo service mysql stop
+sudo service mysql start
